e884d1ae6f382cce0e9188aa1944730baba13f4b
Dev Khant <devkhant24@gmail.com>
2023-08-08 21:25:58 +0530

    change in local URL

--- frontend/retreive.js
+++ frontend/retreive.js
@@ -30,7 +30,7 @@ function displayOpenAIKey(openaiKey, currentPageLink) {
     var summaryContainer = document.getElementById('summary-container');
 
     if (openaiKey) {
-        fetch('http://localhost:8000/', {
+        fetch('http://localhost:8000', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
